6d875fecb39560498fcbcc23f40222a68ca914ef
Hari Venkata Sai Ganesh Lolla <20100850@mail.wit.ie>
2023-03-22 18:31:21 +0000

    Exercise 2 completed

--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -1,10 +1,44 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
 const Demo = () => {
+  let modules = [
+    {
+      m_name: "Enterprise Web Dev",
+      m_nolectures: 2,
+      m_nopracticles: 2,
+    },
+    {
+      m_name: "Data Mining",
+      m_nolectures: 3,
+      m_nopracticles: 4,
+    },
+  ];
+
   return (
-      <h1>TODO</h1>
+    <div>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name of the course</th>
+            <th>No of lectures</th>
+            <th>No of practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{modules[0].m_name}</td>
+            <td>{modules[0].m_nolectures}</td>
+            <td>{modules[0].m_nopracticles}</td>
+          </tr>
+          <tr>
+            <td>{modules[1].m_name}</td>
+            <td>{modules[1].m_nolectures}</td>
+            <td>{modules[1].m_nopracticles}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
